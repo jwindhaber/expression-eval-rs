31e4a91d77a0117ff572021e8ed4cb2399deed5a
jwindhaber <juergen.windhaber@gmail.com>
2021-12-07 14:26:38 +0100

    cargo files and lib root added

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+mod tokenizer;
+mod eval;
+mod converter;
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
